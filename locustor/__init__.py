6f03616034b1c4242aa4e90c05d993fc7dbe2f46
jamatute <jamatute@paradigmadigital.com>
2017-10-20 13:54:40 +0200

    [fix] typo

--- locustor/__init__.py
+++ locustor/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # locustor: Tool to execute locust and produce reports
 #
-# Copyright (C) 2017 jamatute <jmm@riseup.net>
+# Copyright (C) 2017 jamatute <jamatute@paradigmadigital.com>
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
