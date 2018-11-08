e7637ac2d9489aaad7f3ef8bf96638f1db624a47
Ruben Vos <rvos@paradigmadigital.com>
2018-11-08 16:24:34 +0100

    update host example 2

--- examples/example1.py
+++ examples/example1.py
@@ -2,8 +2,7 @@ import os
 
 from locustor.locustor import Locustor
 
-locustor = Locustor(host='http://pre-saigon-int.lowi.es:8080/bundle',
-                    locust_file='../locustlowi/locustfile.py',
+locustor = Locustor(host='http://example:8080/example',
                     work_dir=os.path.expanduser('csv'),
                     num_clients=10,
                     hatch_rate=50,
